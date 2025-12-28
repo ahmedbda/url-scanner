a4478d4aec1c5547b6d87ce1a56d93c23c8293ab
ahmedbda <prsmul@pm.me>
2025-12-28 18:08:45 +0100

    added header list and header scanning (+ return)

--- src/scanners/header_scanner.py
+++ src/scanners/header_scanner.py
@@ -4,11 +4,22 @@ class HeaderScanner():
     def scan(self, url: str):
         # changing the "python-httpx" default header for privacy and professionalism
         headers_request = {"User-Agent": "url-scanner/1.0"}
+        headers_to_scan = ["hsts", "Strict-Transport-Security", "Content-Security-Policy", "X-Frame-Options"]
+        analysis_results = {}
 
         try:
             response = httpx.get(url, headers=headers_request, timeout=5) # 5 second timeout to not get flagged
-            return response.headers
+            received_headers = response.headers
         
         except httpx.RequestError as exc:
-            print(f"Error: {exc.request.url!r}.")
+            print(f"Error connecting to {url}: {exc}")
             return None
+        
+        for header in headers_to_scan:
+                if header in received_headers:
+                    analysis_results[header] = received_headers.get(header)
+
+                else:
+                    analysis_results[header] = "Missing"
+        
+        return analysis_results
